05dbc475f574ff74cf36d7d4b593077d82fd3a66
Shil <aurelie.beninca@gmail.com>
2023-10-30 00:09:56 +0100

    add service picture

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,6 +12,10 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
