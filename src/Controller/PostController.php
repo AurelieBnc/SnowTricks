c1d3febb9327c66bca643adde7884d50a1a6204b
Shil <aurelie.beninca@gmail.com>
2023-11-05 21:55:47 +0100

    add method editPost & editMedia on PostController + refacto edit_post Template & index post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -12,6 +12,7 @@ use App\Entity\Comment;
 use App\Entity\Picture;
 use App\Form\CommentType;
 use App\Form\CreatePostFormType;
+use App\Form\MediaType;
 use App\Form\PictureType;
 use App\Service\PictureService;
 use Symfony\Component\HttpFoundation\Request;
@@ -120,6 +121,133 @@ class PostController extends AbstractController
 
     }
 
+    #[Route('/post/{post_id}/edit/media/{id}', name: 'post_edit_media')]
+    public function editMedia(int $post_id, int $id, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        //todo flusher l'url media au début pour éviter de la modifier a chaque display
+        $user = $this->getUser();
+
+        $postRepo = $entityManager->getRepository(Post::class);
+        $post = $postRepo->find($post_id);
+
+        $mediaRepo = $entityManager->getRepository(Media::class);
+        $media = $mediaRepo->find($id);
+
+        $editMedia = new Media;
+
+        $mediaForm = $this->createForm(MediaType::class, $editMedia);
+        $mediaForm->handleRequest($request);
+
+        if ($mediaForm->isSubmitted() && $mediaForm->isValid()) {
+            if ($user) {
+                if ($user->isVerified() === false) {
+                    $this->addFlash('verification', 'Vous devez confirmer votre adresse email.');
+                    $this->redirectToRoute('app_home', [
+                    ]);
+                }
+
+                $modifyUrl = str_replace('youtu.be', 'youtube.com/embed', $editMedia->getVideoUrl());
+                $media->setVideoUrl($modifyUrl); 
+
+                // $mediaData = $mediaForm->get('videoUrl')->getData();
+
+                // $media->setvideoUrl($mediaData);
+                $media->setPost($post);
+                $post->addMedia($media);
+
+                $entityManager->persist($media);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('post_edit', [
+                    'id' => $post->getId(),
+                ]);
+            } else {
+                $this->addFlash('login', 'Vous devez être connecté pour modifier un lien.');
+            }
+        }
+
+        $modifyUrl = str_replace('youtu.be', 'youtube.com/embed', $media->getVideoUrl());
+        $media->setVideoUrl($modifyUrl);
+
+        return $this->render('post/edit/edit_media.html.twig', [
+            'form' => $mediaForm,
+            'media' => $media,
+        ]);
+
+    }
+
+    #[Route('/post/{id}/edit', name: 'post_edit')]
+    public function editPost(int $id, Request $request, EntityManagerInterface $entityManager, PictureService $pictureService): Response
+    {
+        $pictureList = null;
+        $videoUrlList = null;
+        $commentList = null;
+
+        $postRepo = $entityManager->getRepository(Post::class);
+        $post = $postRepo->find($id);
+
+        $mediaRepo = $entityManager->getRepository(Media::class);
+        $videoUrlList = $mediaRepo->videoUrlList($post->getId());
+
+        $pictureRepo = $entityManager->getRepository(Picture::class);
+        $pictureList = $pictureRepo->pictureList($post->getId()); 
+
+        // Video url display processing
+        $urlModifiedList = [];
+        foreach ($videoUrlList as $url) {
+            
+            $modifyUrl = str_replace('youtu.be', 'youtube.com/embed', $url->getVideoUrl());
+            $url->setVideoUrl($modifyUrl);
+        }
+
+        $postForm = $this->createForm(CreatePostFormType::class, $post);
+        $postForm->handleRequest($request);
+
+        if ($postForm->isSubmitted() && $postForm->isValid()) {
+            if ($this->getUser()) {
+                // if ($this->getUser()->is_granted('ROLE_ADMIN') === false) {
+                //     $this->addFlash('verification', 'Vous devez être administrateur pour créer un article.');
+                //     $this->redirectToRoute('login');
+                // }
+
+                $post->setCreatedAt(new \DateTimeImmutable()); 
+                
+                $pictureList = $postForm->get('pictureList')->getData();
+                
+                foreach ($pictureList as $picture) {
+                    $folder = 'postImages';
+                    $field = $pictureService->add($picture, $folder);
+
+                    $picture = new Picture;
+                    $picture->setName($field);
+                    $post->addPicture($picture);
+                }
+
+                $media = new Media;
+                $url = $postForm->get('media')->getData();
+                $media->setVideoUrl($url);
+                $post->addMedia($media);
+
+                $entityManager->persist($post);
+                $entityManager->flush();
+
+                $this->addFlash('success', 'Ton article a bien été ajouté !');
+
+                return $this->redirectToRoute('app_home');
+            } else {
+                $this->addFlash('login', 'Vous devez être connecté pour écrire un article.');
+            }
+        }
+
+        return $this->render('post/edit/edit_post.html.twig', [
+            'post' => $post,
+            'pictureList' => $pictureList,
+            'videoUrlList' => $urlModifiedList,
+            'mediaList' => $videoUrlList,
+            'commentList' => $commentList,
+            'form' => $postForm,
+        ]);
+    }
 
     #[Route('/post/{id}/{loader}', name: 'post')]
     public function index(int $id, int $loader, EntityManagerInterface $entityManager, Request $request, ?UserInterface $user): Response
@@ -153,7 +281,7 @@ class PostController extends AbstractController
         // Video url display processing
         $urlModifiedList = [];
         foreach ($videoUrlList as $url) {
-            $urlModifiedList [] = str_replace('youtu.be', 'youtube.com/embed', $url);
+            $urlModifiedList [] = str_replace('youtu.be', 'youtube.com/embed', $url->getVideoUrl());
         }
 
         // Create a commentForm
