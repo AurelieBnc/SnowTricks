797272d0ed7a6856170c66f67d9f1e523a33c60a
Shil <aurelie.beninca@gmail.com>
2023-11-20 23:44:41 +0100

    add pagination method's comment

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Comment;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -62,32 +63,33 @@ class CommentRepository extends ServiceEntityRepository
     }
 
     /**
-     * return the list of the first ten comments of a trick
-     * @return array<Comment>
+     * returns comments paginated
      */
-    public function commentListMaxTen(int $idTrick) : array
+    public function findCommentListPaginated(int $page, int $idTrick, int $limit = 10): array
     {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.trick = :val')
-            ->setParameter('val', $idTrick)
-            ->orderBy('c.createdAt', 'DESC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-       ;
-    }
+        $limit = abs($limit);
 
-    /**
-     * returns the total number of items
-     */
-    public function countCommentList(int $idTrick): int
-    {
-       $result = $this->createQueryBuilder('c')
-            ->andWhere('c.trick = :val')
-            ->setParameter('val', $idTrick)
-            ->getQuery()
-            ->getResult()
-            ;
-        return count($result);
+        $result = [];
+
+        $query = $this->createQueryBuilder('c')
+        ->andWhere('c.trick = :val')
+        ->setParameter('val', $idTrick)
+        ->orderBy('c.createdAt', 'DESC')
+        ->setMaxResults($limit)
+        ->setFirstResult($page * $limit - $limit);
+
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        if (empty($data)) {
+            return $result;
+        }
+        $pages = ceil($paginator->count() / $limit);
+        $result['data'] = $data;
+        $result['pages'] = $pages;
+        $result['page'] = $page;
+        $result['limit'] = $limit;
+
+        return $result;
     }
 }
